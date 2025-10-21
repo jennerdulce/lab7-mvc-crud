8743714dde8a477ae340e3c5bb373a3f9ad1121d
jennerdulce <jennerdulce@gmail.com>
2025-10-20 21:10:34 -0700

    fix: all parts able to sync and initialize on app.js rendering on the #app div

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,20 @@
-import model from "model.js"
-import view from "view.js"
-import contoller from "contoller.js"
-import eliza from "contoller.js"
+import { SimpleChatModel } from "./model.js"
+import { SimpleChatView } from "./view.js"
+import { SimpleChatController } from "./controller.js"
 
+function initializeApp() {
+    document.addEventListener('DOMContentLoaded', () => {
+        console.log('Initialize Simple Chat...');
 
+        const model = new SimpleChatModel();
+        const view = new SimpleChatView();
+
+        view.render('#app');
+
+        const controller = new SimpleChatController(model, view);
+        controller.init();
+    });
+}
+
+// Actually call the initialize function
+initializeApp();
